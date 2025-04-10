31336101b194dc93d903829d49dbb51ee1371686
ssp <qkrtjdtlr02@naver.com>
2025-04-10 13:21:02 +0900

    배포 셋팅

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 
 export default mergeConfig(
   defineConfig({
+    base: "/front-5th-chapter1-3/",
     plugins: [react()],
   }),
   defineTestConfig({
